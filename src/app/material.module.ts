c8176cdcdf1775a46b496129a86bfcdf97da15b7
Jose-Antonio Rubio <jose-antonio.rubio@iii.com>
2021-06-01 16:18:09 -0400

    added in basic routing

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from "@angular/core";
-import { MatSliderModule } from '@angular/material/slider';
-
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  imports: [],
-  exports:[MatSliderModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports:[
+    MatButtonModule,
+    MatIconModule,
+  ]
 })
 export class MaterialModule {}
