176ec05f878236aa34ae0189d71d1d3f2de497ae
Jose-Antonio Rubio <jose-antonio.rubio@iii.com>
2021-06-04 19:19:46 -0400

    tracking exercises and displaying in table

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
 @NgModule({
   imports: [
     MatButtonModule,
@@ -31,6 +32,7 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatSelectModule,
     MatProgressSpinnerModule,
     MatDialogModule,
+    MatTableModule,
   ],
   exports:[
     MatButtonModule,
@@ -48,6 +50,7 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatSelectModule,
     MatProgressSpinnerModule,
     MatDialogModule,
+    MatTableModule,
   ]
 })
 export class MaterialModule {}
