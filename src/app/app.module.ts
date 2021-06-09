3a25a93c0b33154c7983b6fae4d417d63159f5d3
Jose-Antonio Rubio <jadrubio@gmail.com>
2021-06-08 21:12:22 -0400

    separated out modules and added lazyloading for trainig component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingComponent } from './training/past-training/past-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavComponent } from './navigation/sidenav/sidenav.component';
-import { StopTrainingComponent } from './training/current-training/stop-training-component';
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore'
-import { environment } from 'src/environments/environment';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 import { UIService } from './shared/ui-service';
-
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavComponent,
-    StopTrainingComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    AppRoutingModule,
     FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
+    AuthModule,
+    AngularFirestoreModule
   ],
   providers: [AuthService, TrainingService, UIService],
-  bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,23 @@
+import { Route } from "@angular/compiler/src/core";
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, CanLoad } from "@angular/router";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate, CanLoad{
   constructor(private authService: AuthService, private router: Router){
 
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    if (this.authService.isAuth()){
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  canLoad(route: Route){
     if (this.authService.isAuth()){
       return true;
     } else {
